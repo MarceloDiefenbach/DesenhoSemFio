69deb0434f4b6a2d1e678a746bb7daf46fa8b886
Marcelo Diefenbach <58398373+MarceloDiefenbach@users.noreply.github.com>
2022-10-17 08:48:11 -0300

    Initial Commit

---
+++ DesenhoSemFio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesenhoSemFio
+//
+//  Created by Marcelo Diefenbach on 17/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesenhoSemFio/DesenhoSemFioApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesenhoSemFioApp.swift
+//  DesenhoSemFio
+//
+//  Created by Marcelo Diefenbach on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DesenhoSemFioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
